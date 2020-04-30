6d7fcc3c9e4b7766695228500f1325b8d12f8340
Dan Laine <daniel.laine@avalabs.org>
2020-04-30 17:56:01 -0400

    change permissions on salticidae-go. bump salticidae-go version

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@
 echo "Fetching dependencies..."
 go mod download
 
-SALTICIDAE_GO_VER="v0.1.0" # Must match salticidae-go version in go.mod
+SALTICIDAE_GO_VER="v0.2.0" # Must match salticidae-go version in go.mod
 export SALTICIDAE_GO_PATH=$GOPATH/pkg/mod/github.com/ava-labs/salticidae-go@$SALTICIDAE_GO_VER
 
 # This script depends on CGO_CFLAGS and CGO_LDFLAGS, which are exported from the below env file.
@@ -13,6 +13,7 @@ source ${SALTICIDAE_GO_PATH}/scripts/env.sh
 
 # Build salticidae-go
 echo "Building salticidae-go..."
+chmod -R 744 $SALTICIDAE_GO_PATH
 bash $SALTICIDAE_GO_PATH/scripts/build.sh
 
 # Build the binaries
