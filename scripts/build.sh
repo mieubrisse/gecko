0243916cb3a74aa4ebceb63a8db90ed34863f5bf
Dan Laine <daniel.laine@avalabs.org>
2020-04-30 17:20:09 -0400

    change order of build script to ensure dependencies exist before using them

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# Fetch Gecko dependencies, including salticidae-go and coreth
+echo "Fetching dependencies..."
+go mod download
+
 SALTICIDAE_GO_VER="v0.1.0" # Must match salticidae-go version in go.mod
 export SALTICIDAE_GO_PATH=$GOPATH/pkg/mod/github.com/ava-labs/salticidae-go@$SALTICIDAE_GO_VER
 
@@ -7,20 +11,12 @@ export SALTICIDAE_GO_PATH=$GOPATH/pkg/mod/github.com/ava-labs/salticidae-go@$SAL
 # Those variables specify C dependencies of Gecko.
 source ${SALTICIDAE_GO_PATH}/scripts/env.sh
 
-CORETH_VER="v0.1.0" # Must match coreth version in go.mod
-CORETH_PATH=$GOPATH/pkg/mod/github.com/ava-labs/coreth@$CORETH_VER
-
-# Fetch Gecko dependencies, including salticidae-go and coreth
-echo "Fetching dependencies..."
-go mod download
-
 # Build salticidae-go
 echo "Building salticidae-go..."
 bash $SALTICIDAE_GO_PATH/scripts/build.sh
 
 # Build the binaries
-# Directory above this build script
-GECKO_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
+GECKO_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd ) # Directory above this script
 
 BUILD_DIR="${GECKO_PATH}/build"
 
@@ -31,4 +27,6 @@ echo "Building throughput test binary..."
 go build -o "$BUILD_DIR/xputtest" "$GECKO_PATH/xputtest/"*.go
 
 echo "Building EVM plugin binary..."
+CORETH_VER="v0.1.0" # Must match coreth version in go.mod
+CORETH_PATH=$GOPATH/pkg/mod/github.com/ava-labs/coreth@$CORETH_VER
 go build -o "$BUILD_DIR/plugins/evm" "$CORETH_PATH/plugin/"*.go
