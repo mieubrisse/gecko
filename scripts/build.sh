696981d5134d27e4a931eca22b90871ccb5cd229
Dan Laine <daniel.laine@avalabs.org>
2020-04-30 14:37:09 -0400

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,36 +1,34 @@
 #!/bin/bash -e
 
-# Ted: contact me when you make any changes
+SALTICIDAE_GO_VER="v0.1.0" # Must match salticidae-go version in go.mod
+export SALTICIDAE_GO_PATH=$GOPATH/pkg/mod/github.com/ava-labs/salticidae-go@$SALTICIDAE_GO_VER
 
-PREFIX="${PREFIX:-$(pwd)/build}"
-PLUGIN_PREFIX="$PREFIX/plugins"
+# This script depends on CGO_CFLAGS and CGO_LDFLAGS, which are exported from the below env file.
+# Those variables specify C dependencies of Gecko.
+source ${SALTICIDAE_GO_PATH}/scripts/env.sh
 
-SRC_DIR="$(dirname "${BASH_SOURCE[0]}")"
-source "$SRC_DIR/env.sh"
+CORETH_VER="v0.1.0" # Must match coreth version in go.mod
+CORETH_PATH=$GOPATH/pkg/mod/github.com/ava-labs/coreth@$CORETH_VER
 
-CORETH_PKG=github.com/ava-labs/coreth
-CORETH_PATH="$GOPATH/src/$CORETH_PKG"
-if [[ -d "$CORETH_PATH/.git" ]]; then
-    cd "$CORETH_PATH"
-    go get -t -v -d "./..."
-    cd -
-else
-    go get -t -v -d "$CORETH_PKG/..."
-fi
-cd "$CORETH_PATH"
-git -c advice.detachedHead=false checkout v0.1.0
-cd -
+# Fetch Gecko dependencies, including salticidae-go and coreth
+echo "Fetching dependencies..."
+go mod download
 
-GECKO_PKG=github.com/ava-labs/gecko
-GECKO_PATH="$GOPATH/src/$GECKO_PKG"
-if [[ -d "$GECKO_PATH/.git" ]]; then
-    cd "$GECKO_PATH"
-    go get -t -v -d "./..."
-    cd -
-else
-    go get -t -v -d "$GECKO_PKG/..."
-fi
+# Build salticidae-go
+echo "Building salticidae-go..."
+bash $SALTICIDAE_GO_PATH/scripts/build.sh
 
-go build -o "$PREFIX/ava" "$GECKO_PATH/main/"*.go
-go build -o "$PREFIX/xputtest" "$GECKO_PATH/xputtest/"*.go
-go build -o "$PLUGIN_PREFIX/evm" "$CORETH_PATH/plugin/"*.go
+# Build the binaries
+# Directory above this build script
+GECKO_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
+
+BUILD_DIR="${GECKO_PATH}/build"
+
+echo "Building Gecko binary..."
+go build -o "$BUILD_DIR/ava" "$GECKO_PATH/main/"*.go
+
+echo "Building throughput test binary..."
+go build -o "$BUILD_DIR/xputtest" "$GECKO_PATH/xputtest/"*.go
+
+echo "Building EVM plugin binary..."
+go build -o "$BUILD_DIR/plugins/evm" "$CORETH_PATH/plugin/"*.go
